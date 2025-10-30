fec1a12fc442a652b43948a1c66a11e112293a57
ODiegoPA <diegoperezanez@gmail.com>
2025-10-30 15:12:32 -0400

    Bug fixeado

--- src/main/java/com/example/demo/infraestructure/adapters/Jpa/JpaRepository/LoteSucursalJpaRepository.java
+++ src/main/java/com/example/demo/infraestructure/adapters/Jpa/JpaRepository/LoteSucursalJpaRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.infraestructure.adapters.entity.LoteSucursalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoteSucursalJpaRepository extends JpaRepository<LoteSucursalEntity, Long> {
+    Optional<LoteSucursalEntity> findByLoteIdAndSucursalId(Long loteId, Long sucursalId);
 }
